9b3a30f96889434377b2db64b60665b9b37d1e92
DylanDrechsel <DylanDrechsel@gmail.com>
2021-12-23 13:24:35 -0500

    Completed the input fields and got all the data needed to make the create accident request

---
+++ Pages/ReportAnAccidentPage/ReportAnAccidentComponents/InputFieldsComponents/PackageNumberInput.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, TextInput } from 'react-native'
+import { SubmitStyle } from "../../../../Styles/ReportAnAccidentStyles";
+
+
+const PackageNumberInput = ({ handleInput }) => {
+
+
+    return (
+        <View style={SubmitStyle.container}>
+            <Text style={SubmitStyle.text}> How many packages were you delivering? </Text>
+                <TextInput 
+                    name='PackageNumber'
+                    placeholder=" Number of packages carried"
+                    style={SubmitStyle.input}
+                    onChangeText={(packageNumber) => {
+                        handleInput('numberPackageCarried', packageNumber)
+                    }}
+                />
+        </View>
+    )
+}
+
+export default PackageNumberInput
