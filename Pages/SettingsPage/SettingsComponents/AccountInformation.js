6839ab7fb0a3ebf3ca8a9c44269c7d31766c219e
NLanese <NLanese21@gmail.com>
2021-12-29 13:36:19 -0500

    final basic touches, still need to move onto the next step of functionality

--- Pages/SettingsPage/SettingsComponents/AccountInformation.js
+++ Pages/SettingsPage/SettingsComponents/AccountInformation.js
@@ -3,15 +3,28 @@ import { View, Text } from 'react-native'
 import AdminAndUserInformation from "./InformationComponents/AdminAndUserInformation";
 import EditAccountInformationButton from "./ButtonBoxComponents/EditAccountInformationButton";
 import ViewAccidentsButton from "./ButtonBoxComponents/ViewAccidentsButton";
+import { useQuery } from "@apollo/client";
 import { settings } from "../../../Styles/SettingStyles";
 
 const AccountInformation = (props) => {
 
+    const [userData, { loading: loading, error: error, data: data }] = useQuery(GET_USER_DATA);
+    /* Going to be used to check for errors */
+    useEffect(() => {
+        if (!loading && data) {
+            console.log(data)
+        } else {
+            console.log(error)
+        }
+    }, [dataS, errorS]);
+
     return (
         <View style={settings.container}>
-            <AdminAndUserInformation />
-            <EditAccountInformationButton />
-            <ViewAccidentsButton />
+            <View>
+                <AdminAndUserInformation userData={userData}/>
+            </View>
+            <EditAccountInformationButton userData={userData}/>
+            <ViewAccidentsButton userData={userData}/>
         </View>
     )
 }
