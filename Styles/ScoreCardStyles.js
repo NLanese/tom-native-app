824e8d9c32904d961fe3f4a082b894b61f2427ac
NLanese <NLanese21@gmail.com>
2021-12-31 15:31:43 -0500

    Added button styling to ScoreCard pages

--- Styles/ScoreCardStyles.js
+++ Styles/ScoreCardStyles.js
@@ -17,5 +17,20 @@ const EmployeeSafetyAndComplianceStyles = StyleSheet.create({
 })
 
 const ButtonBoxStyles = StyleSheet.create({
-    
+    ButtonCasing: {
+        marginTop: 55,
+        width: "90%",
+        left: "5%",
+        backgroundColor: '#02020A',
+        borderRadius: 30,
+        borderColor: '#ffffff'
+    }
 })
+
+export { 
+    ScoreCardStyles, 
+    QualityStyles, 
+    SafetyAndComplianceStyles, 
+    EmployeeSafetyAndComplianceStyles, 
+    ButtonBoxStyles
+}
