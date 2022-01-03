53da5ee07f50264a383b91ae41fcde30ef681461
DylanDrechsel <DylanDrechsel@gmail.com>
2022-01-03 14:53:16 -0500

    Completed basic layout for Banner.js

--- Global/Banner.js
+++ Global/Banner.js
@@ -1,61 +1,57 @@
-import * as React from 'react';
-import { Appbar } from 'react-native-paper';
+import React from "react";
+import { Appbar, Avatar } from 'react-native-paper';
 import { useHistory } from 'react-router-native';
 import { StyleSheet, View, Text } from 'react-native';
-import { NavbarStyles } from '../Styles/NavBarStyles';
 
-const BannerComponent = () => (
-    <View>
-        <View style={styles.topBar}></View>
+const BannerComponent = () => {
+    let history = useHistory()
 
-        <Appbar style={styles.bottom}>
-        <Text style={NavbarStyles.leftText} onPress={() => {
-                history.push('/home');
-            }}> Home </Text>
+    return (
+        <View>
+            <View style={styles.topBar}></View>
 
-            <Text style={NavbarStyles.rightText} onPress={() => {
-                history.goBack();
-            }}> Go Back </Text>
-
-            <Appbar.Action
-                color='#d0c1d0'
-                style={styles.actionBar}
-                icon="archive"
-                onPress={() => console.log('Pressed archive')}
-                />
-                <Appbar.Action color='#d0c1d0' style={styles.actionBar} icon="mail" onPress={() => console.log('Pressed mail')} />
-                <Appbar.Action color='#d0c1d0' style={styles.actionBar} icon="label" onPress={() => console.log('Pressed label')} />
+            <Appbar style={styles.bottom}>
                 <Appbar.Action
                     color='#d0c1d0'
                     style={styles.actionBar}
-                    icon="delete"
-                    onPress={() => console.log('Pressed delete')}
-                />
-        </Appbar>
+                    icon="arrow-left-bold"
+                    onPress={() => history.goBack()}
+                  />
+                  
+                  <Appbar.Action
+                      color='#d0c1d0'
+                      style={styles.actionBar}
+                      icon="home"
+                      onPress={() => history.push('/home')}
+                  />
+
+                  <Avatar.Image size={24} source={require('../assets/tom-logo-white-transparent.png')} />
+            </Appbar>
     </View>
- );
+    )
+}
 
 export default BannerComponent
 
 const styles = StyleSheet.create({
-  bottom: {
-    height: 50,
-    position: 'relative',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: '#24296f'
-  },
-  actionBar: {
-    position: 'relative',
-    top: 0,
-    color: '#d0c1d0' 
-  },
-  topBar: {
-    position: 'relative',
-    top: 0,
-    height: 20,
-    backgroundColor: '#9174a9'
-  }
-});
+    bottom: {
+      height: 50,
+      position: 'relative',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: '#24296f'
+    },
+    actionBar: {
+      position: 'relative',
+      top: 0,
+      color: '#d0c1d0' 
+    },
+    topBar: {
+      position: 'relative',
+      top: 0,
+      height: 20,
+      backgroundColor: '#9174a9'
+    }
+  });
