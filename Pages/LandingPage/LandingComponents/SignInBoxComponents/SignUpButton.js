17d36848e8caac96515fd34e0b306417c7456ba1
DylanDrechsel <DylanDrechsel@gmail.com>
2022-01-05 09:21:39 -0500

    seeing changes that were made

--- Pages/LandingPage/LandingComponents/SignInBoxComponents/SignUpButton.js
+++ Pages/LandingPage/LandingComponents/SignInBoxComponents/SignUpButton.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from 'react-native-paper';
-import { View, Modal, TextInput, Text, Pressable, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Modal, TextInput, Text, Pressable, ScrollView, TouchableOpacity, ImageBackground } from 'react-native'
 import { useMutation } from '@apollo/client';
 import { SIGNUP } from '../../../../GraphQL/operations';
 import { ButtonStyles, SignUpModal } from '../../../../Styles/LandingPageStyles';
@@ -43,126 +43,126 @@ const SignUpButton = () => {
 
     return (
 			<View>
-				<Modal animationType='slide' transparent={true} visible={modalVisible} /* style={SignUpModal.modal} */>
 
-					<View style={SignUpModal.centeredView}>
-						<ScrollView style={SignUpModal.modalView}>
+					<Modal animationType='slide' transparent={true} visible={modalVisible} /* style={SignUpModal.modal} */>
 
-							<View style={SignUpModal.insideModalView}>
-								<Text style={SignUpModal.modalTitle}> Tom App Sign Up! </Text>
-								<Text style={SignUpModal.modalSubTitle}> Please enter the proper information below! </Text>
+						<View style={SignUpModal.centeredView}>
+							<ScrollView style={SignUpModal.modalView}>
 
-								<Text style={SignUpModal.modalText}> Email </Text>
-								<TextInput
-									name='Email'
-									style={SignUpModal.input}
-									onChangeText={(email) => {
-										handleInput('email', email);
-									}}
-								/>
+								<View style={SignUpModal.insideModalView}>
+									<Text style={SignUpModal.modalTitle}> Tom App Sign Up! </Text>
+									<Text style={SignUpModal.modalSubTitle}> Please enter the proper information below! </Text>
 
-								<Text style={SignUpModal.modalText}> Username </Text>
-								<TextInput
-									name='Username'
-									style={SignUpModal.input}
-									onChangeText={(username) => {
-										handleInput('username', username);
-									}}
-								/>
-
-								<Text style={SignUpModal.modalText}> First Name </Text>
-								<TextInput
-									name='Firstname'
-									style={SignUpModal.input}
-									onChangeText={(firstname) => {
-										handleInput('firstname', firstname);
-									}}
-								/>
-
-								<Text style={SignUpModal.modalText}> Last Name </Text>
-								<TextInput
-									name='Lastname'
-									style={SignUpModal.input}
-									onChangeText={(lastname) => {
-										handleInput('lastname', lastname);
-									}}
-								/>
-
-								<Text style={SignUpModal.modalText}> Phone Number </Text>
-								<TextInput
-									name='Phone Number'
-									style={SignUpModal.input}
-									onChangeText={(phoneNumber) => {
-										handleInput('phoneNumber', phoneNumber);
-									}}
-								/>
-
-								<Text style={SignUpModal.modalText}> Password </Text>
-								<TextInput
-									name='Password'
-									style={SignUpModal.input}
-									onChangeText={(password) => {
-										handleInput('password', password);
-									}}
-								/>
-
-								<Text style={SignUpModal.modalText}> Admin Email </Text>
-								<TextInput
-									name='Admin Email'
-									style={SignUpModal.input}
-									onChangeText={(adminEmail) => {
-										handleInput('adminEmail', adminEmail);
-									}}
-								/>
-
-								<View style={ButtonStyles.signUpSubmitButton}>
-									<Button
-										title='Submit!'
-										accessibilityLabel='Submit!'
-										loading={buttonLoading}
-										titleStyle={{ fontWeight: '700' }}
-										buttonStyle={{
-										backgroundColor: '#02020A',
-										borderColor: 'transparent',
-										borderWidth: 0,
-										borderRadius: 5,
-										paddingVertical: 5,
-										}}
-										onPress={ async () => {
-											// handleButtonLoading()
-											signup({
-												variables: {
-													signupInput: {
-														email: userData.email,
-														username: userData.username,
-														password: userData.password,
-														firstname: userData.firstname,
-														lastname: userData.lastname,
-														phoneNumber: userData.phoneNumber,
-														adminEmail: userData.adminEmail
-													},
-												},
-											});
-											// setModalVisible(!modalVisible);
+									<Text style={SignUpModal.modalText}> Email </Text>
+									<TextInput
+										name='Email'
+										style={SignUpModal.input}
+										onChangeText={(email) => {
+											handleInput('email', email);
 										}}
 									/>
+
+									<Text style={SignUpModal.modalText}> Username </Text>
+									<TextInput
+										name='Username'
+										style={SignUpModal.input}
+										onChangeText={(username) => {
+											handleInput('username', username);
+										}}
+									/>
+
+									<Text style={SignUpModal.modalText}> First Name </Text>
+									<TextInput
+										name='Firstname'
+										style={SignUpModal.input}
+										onChangeText={(firstname) => {
+											handleInput('firstname', firstname);
+										}}
+									/>
+
+									<Text style={SignUpModal.modalText}> Last Name </Text>
+									<TextInput
+										name='Lastname'
+										style={SignUpModal.input}
+										onChangeText={(lastname) => {
+											handleInput('lastname', lastname);
+										}}
+									/>
+
+									<Text style={SignUpModal.modalText}> Phone Number </Text>
+									<TextInput
+										name='Phone Number'
+										style={SignUpModal.input}
+										onChangeText={(phoneNumber) => {
+											handleInput('phoneNumber', phoneNumber);
+										}}
+									/>
+
+									<Text style={SignUpModal.modalText}> Password </Text>
+									<TextInput
+										name='Password'
+										style={SignUpModal.input}
+										onChangeText={(password) => {
+											handleInput('password', password);
+										}}
+									/>
+
+									<Text style={SignUpModal.modalText}> Admin Email </Text>
+									<TextInput
+										name='Admin Email'
+										style={SignUpModal.input}
+										onChangeText={(adminEmail) => {
+											handleInput('adminEmail', adminEmail);
+										}}
+									/>
+
+									<View style={ButtonStyles.signUpSubmitButton}>
+										<Button
+											title='Submit!'
+											accessibilityLabel='Submit!'
+											loading={buttonLoading}
+											titleStyle={{ fontWeight: '700' }}
+											buttonStyle={{
+											backgroundColor: '#02020A',
+											borderColor: 'transparent',
+											borderWidth: 0,
+											borderRadius: 5,
+											paddingVertical: 5,
+											}}
+											onPress={ async () => {
+												// handleButtonLoading()
+												signup({
+													variables: {
+														signupInput: {
+															email: userData.email,
+															username: userData.username,
+															password: userData.password,
+															firstname: userData.firstname,
+															lastname: userData.lastname,
+															phoneNumber: userData.phoneNumber,
+															adminEmail: userData.adminEmail
+														},
+													},
+												});
+												// setModalVisible(!modalVisible);
+											}}
+										/>
+									</View>
+
 								</View>
 
-							</View>
-
-						</ScrollView>
-					</View>
-				</Modal>
+							</ScrollView>
+						</View>
+					</Modal>
 
 				<View>
 					<Button 
 						icon="pen" 
-						mode="outlined"
 						dark={true}
 						mode="contained"
 						style={ButtonStyles.signUpButton}
 						onPress={() => setModalVisible(!modalVisible)}
-					>
+						>
 							Sign Up
 					</Button>
 				</View>
