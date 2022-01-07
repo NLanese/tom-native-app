46ed479239c7e11cf9e8c2de39691f0baeda000f
NLanese <NLanese21@gmail.com>
2022-01-07 01:38:07 -0500

    added content to Nofication

--- Global/BannerComponents/BellComponents/ViewNotifications.js
+++ Global/BannerComponents/BellComponents/ViewNotifications.js
@@ -1,11 +1,27 @@
 import React from "react"
+import { ScrollView } from "react-native"
 
 
-const ViewNotifications = () => {
+const ViewNotifications = ({notificationMessages}) => {
+
+    const renderNotificationMessages = (array) => {
+
+        let renderedList = array.map( notification => {
+            <Text>Notification Data</Text>
+        })
+        return(
+            <View>
+                <Text>You have {renderedList.length} Notifications</Text>
+                <ScrollView>
+                    {renderedList}
+                </ScrollView>
+            </View>
+        )
+    }
 
     return(
         <View>
-
+            {renderNotificationMessages()}
         </View>
     )
 }
