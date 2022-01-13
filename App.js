fe47f57c24c315def84321eb1a97ec777c4bbefe
DylanDrechsel <DylanDrechsel@gmail.com>
2022-01-13 15:24:58 -0500

    testing nav

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler"
 import React, { useState } from 'react';
 import hidden from './hidden';
 import { Portal, Provider as PaperProvider } from 'react-native-paper';
@@ -39,12 +40,14 @@ import Quality from './Pages/ScoreCardPage/ScoreCardComponents/Quality'
 import SafetyAndCompliance from './Pages/ScoreCardPage/ScoreCardComponents/SafetyAndCompliance'
 import Team from './Pages/ScoreCardPage/ScoreCardComponents/Team'
 import Notifications from './Pages/NotificationPage/Notification'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 let state;
 
 // Create HttpLink for Apollo
 const httpLink = createHttpLink({
-	uri: 'http://192.168.1.62:5001/graphql'
+	uri: 'https://warm-retreat-50469.herokuapp.com//graphql'
 });
 
 // Auth for token
@@ -64,6 +67,8 @@ const client = new ApolloClient({
 	cache: new InMemoryCache(),
 });
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   const [loggedIn, setloggedIn] = useState(false)
 
@@ -73,69 +78,33 @@ export default function App() {
 	}
 
   return (
-    <NativeRouter>
+    <NavigationContainer>
+      <ApolloProvider client={client}>
       <RecoilRoot>
-        <ApolloProvider client={client}>
-          <PaperProvider>
-            <View style={AppStyles.container}>
+        <PaperProvider>
+          <View style={AppStyles.container}>
+      <Stack.Navigator>
+
+          
+          
+          {loggedIn === false ? (<Stack.Screen name="/">
+          {props => <LandingPage handleLoggedIn={handleLoggedIn} />}
+          </Stack.Screen>) : null}
+          
+          {/* {loggedIn === true ? (
+            <Banner handleLoggedIn={handleLoggedIn}/>
+            ) : null} */}
             
-             <View>
-
-                {loggedIn === false ? (<LandingPage handleLoggedIn={handleLoggedIn}/>) : null}
-                
-                {loggedIn === true ? (
-                  <Banner handleLoggedIn={handleLoggedIn}/>
-                ) : null}
-
-                {loggedIn === true ? (
-                  <Switch>
-                    <Route exact path='/home' component={Home} />
-                    <Route exact path='/shift_planner' component={ShiftPlanner} />
-
-                    <Route exact path='/view_notifications' component={Notifications} />
-
-                    <Route exact path='/leadership_notified' component={LeadershipNotified} />
-                      <Route exact path='/police_contacted' component={PoliceContacted} />
-                        <Route exact path='/please_remember' component={PleaseRemember} />
-                          <Route exact path='/before_we_begin' component={BeforeWeBegin} />
-                            <Route exact path='/create_or_add' component={CreateOrAdd} />
-                              <Route exact path='/report_an_accident' component={ReportAnAccident} />
-                        
-                        
-                        
-                        <Route exact path='/reportcollision' component={ReportCollision} />
-                        <Route exact path='/reportinjuryaccident' component={ReportInjuryAccident} />
-                        <Route exact path='/reportpropertyaccident' component={ReportPropertyAccident} />
-                        <Route exact path='/reporthitperson' component={ReportHitPerson} />
-                        <Route exact path='/reportinjuryreport' component={ReportInjuryReport} />
-
-
-                    <Route exact path='/reporting' component={Reporting} />
-                    <Route exact path='/productivity' component={Productivity} /> 
-                    <Route exact path='/admin_messages' component={Communication} />
-                    <Route exact path='/analytics' component={Analytics} />
-
-
-                    <Route exact path='/settings' component={Settings} />
-                      <Route exact path='/account_information' component={AccountInformation} />
-                        <Route exact path='/edit_account_information' component={EditAccountInformation} />
-                        <Route exact path='/view_accidents' component={ViewAccidents} />
-                      <Route exact path='/account_settings' component={AccountSettings} />
-
-                    <Route exact path='/score_card' component={ScoreCard} />
-                      <Route exact path='/quality' component={Quality} />
-                      <Route exact path='/safetyandcompliance' component={SafetyAndCompliance} />
-                      <Route exact path='/team' component={Team} />
-
-                  </Switch>
-                ) : null}
-              <StatusBar style="auto" />
+            {/* <Stack.Screen name="Home" component={Banner} /> */}
+            <Stack.Screen name="home">
+              {props => <Home {...props} />}
+            </Stack.Screen>
+            
+      </Stack.Navigator>
               </View>
-            
-            </View>
-          </PaperProvider>
-        </ApolloProvider>
+            </PaperProvider>
       </RecoilRoot>
-    </NativeRouter>
+    </ApolloProvider>
+    </NavigationContainer>
   );
 }
